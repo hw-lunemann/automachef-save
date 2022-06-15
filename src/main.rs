9ca36ce87d347b63162404752553348243659e13
hw-lunemann <hw.lunemann@gmail.com>
2022-06-15 16:59:29 +0200

    Upgrade clap to 3.2 and be stricter about input flags

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::Context;
-use clap::{ArgGroup, Parser};
+use clap::{Parser, Args};
 use walkdir::WalkDir;
 
 use aes::cipher::{BlockDecryptMut, BlockEncryptMut, KeyIvInit};
@@ -13,60 +13,66 @@ type Iv = [u8; 16];
 
 #[derive(Parser)]
 #[clap(name = "automachef-transfer")]
-#[clap(version = "0.1.0")]
+#[clap(version = "0.2.0")]
 #[clap(
     about = "Decrypt, encrypt and transfer platform and user locked Automachef save files.",
-    long_about = "Automachef by HermesInteractive encrypts it's save files with the user's account ID (Steam, Epic) or a static key (Twitch). The id is then used to name the save directory making it possible to decrypt any Automachef save. Transferring Automachef saves involves first decrypting the directory and then re-encrypting. The newly decrypted/encrypted/transferred save directory will be created alongside the original save directory."
+    long_about = "Automachef by HermesInteractive encrypts it's save files with the user's account ID (Steam, Epic) or a static key (Twitch). The ID is then used to name the save directory making it possible to decrypt any regular Automachef save without supplying the ID. Transferring Automachef saves involves first decrypting the directory and then re-encrypting. The newly decrypted/encrypted/transferred save directory will be created alongside the original save directory."
 )]
-#[clap(group(ArgGroup::new("platforms").args(&["epic", "steam", "twitch"])
-    ))]
-struct Cli {
-    /// Set Epic as target
-    #[clap(display_order(1), long, value_name = "Epic account ID")]
-    epic: Option<String>,
-    /// Set Steam as target
-    #[clap(display_order(2), long, value_name = "Steam ID (SteamID64)")]
-    steam: Option<String>,
-    /// Set Twitch as target
-    #[clap(display_order(3), long)]
-    twitch: bool,
-
-    /// Overwrite save files in the target directory if it already exists.
-    #[clap(long)]
-    force_overwrite: bool,
-
-    #[clap(
-        arg_enum,
-        requires_if("encrypt", "platforms"),
-        requires_if("transfer", "platforms")
-    )]
-    action: Action,
-
-    /// e.g. '%APPDATA%/LocalLow/HermesInteractive/Automachef/Saves/<ID>'
-    #[clap(parse(from_os_str), value_name = "Save Folder")]
-    input: std::path::PathBuf,
+enum Action {
+    Decrypt {
+        #[clap(flatten)]
+        opts: CliOptions,
+    },
+    Encrypt { 
+        #[clap(flatten)]
+        platform: Platform,
+        #[clap(flatten)]
+        opts: CliOptions
+    },
+    Transfer { 
+        #[clap(flatten)]
+        platform: Platform,
+        #[clap(flatten)]
+        opts: CliOptions,
+    }
 }
 
-impl Cli {
-    fn get_id(&self) -> Option<String> {
-        if let Some(id) = &self.epic {
-            Some(id.to_owned())
-        } else if let Some(id) = &self.steam {
-            Some(id.to_owned())
-        } else if self.twitch {
-            //base64::encode(b"ajksh54fdhj432h234jh")
-            Some("YWprc2g1NGZkaGo0MzJoMjM0amg=".to_owned())
-        } else {
-            None
+impl Action {
+    fn get_opts(&self) -> &CliOptions {
+        match &self {
+            Action::Decrypt { opts } | Action::Encrypt { opts, .. } | Action::Transfer { opts, .. } => opts,
         }
     }
 }
 
-#[derive(clap::ArgEnum, Clone)]
-enum Action {
-    Decrypt,
-    Encrypt,
-    Transfer,
+#[derive(Args)]
+struct CliOptions {
+    /// Overwrite save files in the target directory if it already exists.
+    #[clap(value_parser, long)]
+    force_overwrite: bool,
+
+    /// e.g. '%APPDATA%/LocalLow/HermesInteractive/Automachef/Saves/<ID>'
+    #[clap(value_parser, value_name = "Save Folder")]
+    input: std::path::PathBuf,
+}
+
+#[derive(Args)] 
+struct Platform {
+    /// Epic account ID
+    #[clap(value_parser, long, value_name = "ID", display_order(0))]
+    epic: Option<String>,
+    /// Steam accunt ID (SteamID64)
+    #[clap(value_parser, long, value_name = "ID", display_order(1))]
+    steam: Option<String>,
+    /// Twitch
+    #[clap(value_parser, long, display_order(2))]
+    twitch: bool
+}
+
+impl Platform {
+    fn get_target_id(&self) -> &str {
+        self.epic.as_deref().unwrap_or_else(|| self.steam.as_deref().unwrap_or("YWprc2g1NGZkaGo0MzJoMjM0amg="))
+    }
 }
 
 struct EncryptedSave {
@@ -81,10 +87,10 @@ impl EncryptedSave {
     fn decrypt(&self, key: &Key) -> anyhow::Result<DecryptedSave> {
         let mut base64_spec = data_encoding::BASE64.specification();
         base64_spec.ignore.push_str(" \t\r\n");
-        let base = base64_spec.encoding().unwrap();
+        let base = base64_spec.encoding().expect("Bug: misconfigured base64 spec");
         let mut buf = base.decode(self.text())?;
 
-        // guaranteed to fit as base64 > cipher_text >= plaintext
+        // guaranteed to fit as len(base64) > len(cipher_text) >= len(plaintext)
         let plaintext = Aes256CbcDec::new(key.into(), self.iv().into())
             .decrypt_padded_mut::<aes::cipher::block_padding::ZeroPadding>(&mut buf)?;
         let new_len = plaintext.len();
@@ -102,12 +108,6 @@ impl EncryptedSave {
 
     fn text(&self) -> &[u8] {
         &self.data[6 + 16..]
-    }
-}
-
-impl std::fmt::Display for EncryptedSave {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{}", String::from_utf8_lossy(&self.data))
     }
 }
 
@@ -137,12 +137,6 @@ impl DecryptedSave {
     }
 }
 
-impl std::fmt::Display for DecryptedSave {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{}", String::from_utf8_lossy(&self.text))
-    }
-}
-
 struct SaveDir<'a> {
     key: Option<Key>,
     dir: &'a std::path::Path,
@@ -155,6 +149,7 @@ fn generate_password(id: &str) -> String {
 fn pbkdf1(password: &str) -> Key {
     let salt = "the salty tears provide thy nourishment";
     let iterations = 2;
+    let key_len = 32;
     let mut last_hash = (password.to_owned() + salt).into_bytes();
     for _ in 0..iterations - 1 {
         last_hash = Sha1::digest(&last_hash).to_vec();
@@ -162,14 +157,14 @@ fn pbkdf1(password: &str) -> Key {
     let mut bytes = Sha1::digest(&last_hash).to_vec();
     let mut ctrl = 1usize;
 
-    while bytes.len() < 32 {
+    while bytes.len() < key_len {
         let mut bytes2 = ctrl.to_string().into_bytes();
         bytes2.append(&mut last_hash);
         bytes.append(&mut Sha1::digest(&mut bytes2).to_vec());
         ctrl += 1;
     }
 
-    bytes[0..32].try_into().unwrap()
+    bytes[0..key_len].try_into().unwrap()
 }
 
 fn process_entry(
@@ -179,23 +174,23 @@ fn process_entry(
     target: &SaveDir,
 ) -> anyhow::Result<()> {
     if entry.path().is_dir() {
-        return anyhow::Ok(());
+        return Ok(());
     }
     let data = std::fs::read(entry.path()).with_context(|| "Failed to read file.")?;
 
     let converted_data = match action {
-        Action::Decrypt => {
+        Action::Decrypt { .. } => {
             let save = EncryptedSave::new(data);
             save.decrypt(&source.key.expect("Action without source key"))
                 .with_context(|| "Failed to decrypt save.")?
-                .to_string()
+                .text
         }
-        Action::Encrypt => {
+        Action::Encrypt { .. } => {
             let save = DecryptedSave::new(data);
             save.encrypt(&target.key.expect("Encryption without target key"), None)
-                .to_string()
+                .data
         }
-        Action::Transfer => {
+        Action::Transfer { .. } => {
             let save = EncryptedSave::new(data);
             let decrypted = save
                 .decrypt(&source.key.expect("Transfer without source key"))
@@ -205,7 +200,7 @@ fn process_entry(
                     &target.key.expect("Transfer without source key"),
                     Some(save.iv()),
                 )
-                .to_string()
+                .data
         }
     };
 
@@ -213,57 +208,71 @@ fn process_entry(
         .into_path()
         .iter()
         .map(|segment| {
-            if segment == source.dir.file_name().expect("Action without source") {
-                target.dir.file_name().expect("Action without target")
+            if segment == source.dir.file_name().expect("Bug: Action without source") {
+                target.dir.file_name().expect("Bug: Action without target")
             } else {
                 segment
             }
         })
         .collect::<std::path::PathBuf>();
 
-    if let Some(parent) = &target_file.parent() {
-        std::fs::create_dir_all(parent)
-            .with_context(|| format!("Failed to create dir: {}", parent.display()))?;
-    }
-
+    let parent_dir = &target_file.parent().context("Bug: Target file path is malformed")?;
+    std::fs::create_dir_all(parent_dir)
+            .with_context(|| format!("Failed to create dir: {}", parent_dir.display()))?;
     std::fs::write(&target_file, converted_data)
         .with_context(|| format!("Failed to write file: {}", &target_file.display()))
 }
 
 fn main() -> anyhow::Result<()> {
-    let mut cli = Cli::parse();
+    let action = Action::parse();
+    let opts = action.get_opts();
 
-    let source_id = cli.input.file_name().and_then(|id| id.to_str()).unwrap();
-    let source = SaveDir {
-        key: Some(pbkdf1(&generate_password(source_id))),
-        dir: &cli.input.to_owned(),
-    };
-
-    if source_id == "Saves" {
-        anyhow::bail!("Error: choose a subdirectory of 'Saves'")
+    if !opts.input.exists() {
+        anyhow::bail!("input doesn't exist")
     }
 
-    let target_id = cli.get_id();
-    let target = SaveDir {
-        key: target_id.as_ref().map(|id| pbkdf1(&generate_password(id))),
-        dir: {
-            let dir_name = match cli.action {
-                Action::Decrypt => source_id.to_owned() + "_decrypted",
-                Action::Encrypt => target_id.expect("encryption without target"),
-                Action::Transfer => target_id.expect("transfer without target"),
-            };
-            cli.input.set_file_name(dir_name);
-            &cli.input
+    if !opts.input.is_dir() {
+        anyhow::bail!("input has to be a directory")
+    }
+
+    if std::ffi::OsStr::new("Saves") == opts.input.file_name().unwrap() {
+        anyhow::bail!("choose a subdirectory of 'Saves'")
+    }
+
+    let source_id = opts.input.file_name().and_then(|id| id.to_str()).context("the input is not a save directory")?;
+    let source = SaveDir {
+        key: match action {
+            Action::Decrypt { .. } | Action::Transfer { .. }=> Some(pbkdf1(&generate_password(source_id))),
+            Action::Encrypt { .. } => None
         },
+        dir: &opts.input.to_owned(),
     };
 
-    if !cli.force_overwrite && target.dir.exists() {
-        anyhow::bail!("Error: target directory {} already exists. Run again with '--force-overwrite' to overwrite the contents.", target.dir.display())
+    let mut target_dir = opts.input.to_owned();
+    let target = match &action {
+        Action::Decrypt { .. } => SaveDir {
+            key: None,
+            dir: {
+                target_dir.set_extension("decrypted");
+                &target_dir
+            }
+        },
+        Action::Encrypt { platform, .. } | Action::Transfer { platform, .. } =>  SaveDir {
+            key: Some(pbkdf1(&generate_password(platform.get_target_id()))),
+            dir: {
+                target_dir.set_file_name(platform.get_target_id());
+                &target_dir
+            }
+        }
+    };
+
+    if !opts.force_overwrite && target.dir.exists() {
+        anyhow::bail!("target directory {} already exists. Run again with '--force-overwrite' to overwrite the contents.", target.dir.display())
     }
 
     for entry in WalkDir::new(&source.dir).into_iter().flatten() {
         println!("{}...", entry.path().display());
-        if let Err(error) = process_entry(entry, &cli.action, &source, &target) {
+        if let Err(error) = process_entry(entry, &action, &source, &target) {
             eprintln!("{}", error);
         }
     }
